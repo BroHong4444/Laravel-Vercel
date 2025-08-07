24b2a712b22c3ba2ce90aa8181299821ec434b6e
Heng Hong <hongofficial4444@gmail.com>
2025-08-07 17:51:44 +0700

    post method new test

--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,35 @@
 
 use App\Http\Controllers\BenchmarkController;
 use App\Models\User;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return response()->json(['message' => 'Hello World!']);
-});
+// Route::get('/hello', function () {
+//     return response()->json(['message' => 'Hello World!']);
+// });
 
-Route::post('/post-method', function () {
-    return ['message' => 'This is post method'];
-});
+// Route::post('/post-method', function () {
+//     return ['message' => 'This is post method'];
+// });
 
-Route::get('/hello', function () {
-    return response()->json(['message' => 'Hello World!']);
-});
+// Route::get('/hello', function () {
+//     return response()->json(['message' => 'Hello World!']);
+// });
 
-Route::get('/users', function () {
-    return response()->json(['users' => User::all()]);
-});
+// Route::get('/users', function () {
+//     return response()->json(['users' => User::all()]);
+// });
 
-Route::prefix('benchmark')->group(function () {
-    Route::get('/users/{iterations?}', [BenchmarkController::class, 'users']);
+// Route::prefix('benchmark')->group(function () {
+//     Route::get('/users/{iterations?}', [BenchmarkController::class, 'users']);
+// });
+
+// Route::post('/send-report', [ReportController::class, 'sendToTelegram']);
+
+Route::post('/test-post', function (Request $request) {
+    return response()->json([
+        'success' => true,
+        'message' => 'POST request received!',
+        'data' => $request->all()
+    ]);
 });
