70ab73daaf8ff0ab53cd762e8a81dfaf9c171ff9
Heng Hong <hongofficial4444@gmail.com>
2025-08-07 17:44:31 +0700

    post method testing

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,14 @@ Route::get('/hello', function () {
     return response()->json(['message' => 'Hello World!']);
 });
 
+Route::post('/post-method', function () {
+    return ['message' => 'This is post method'];
+});
+
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello World!']);
+});
+
 Route::get('/users', function () {
     return response()->json(['users' => User::all()]);
 });
