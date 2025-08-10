919dd00b5dd6c899112415c058e0ac0b4c5214ca
Heng Hong <hongofficial4444@gmail.com>
2025-08-10 11:33:44 +0700

    update api url

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://send-report.vercel.app', 'http://localhost:3000'],
+    'allowed_origins' => ['https://laravel-vercel-bice-pi.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
